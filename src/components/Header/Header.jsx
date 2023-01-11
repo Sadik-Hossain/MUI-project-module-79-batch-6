c02a0b0a1daf8eb371539fedc021d9fa3f8b3ed1
sadik <po480373@gmai.com>
2023-01-11 23:03:58 +0600

    appbar style

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,135 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { BiMenu } from "react-icons/bi";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Container } from "@mui/material";
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
+
+function Header(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      {/* <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography> */}
+      <img
+        width="124"
+        height="44"
+        src="../../../public/logo.png"
+        alt=""
+        style={{ marginTop: "1rem" }}
+      />
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        sx={{
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          mt: 1,
+        }}
+      >
+        <Container>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <BiMenu />
+            </IconButton>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
+              <img
+                width="128"
+                height="40"
+                src="../../../public/logo.png"
+                alt=""
+              />
+            </Box>
+
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item}
+                  sx={{ color: "primary.main" }}
+                  variant="text"
+                >
+                  {item}
+                </Button>
+              ))}
+              <Button sx={{ bgcolor: "primary.green" }}>Login</Button>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {/* 
+      //* -------- mobile menu ------------------
+      */}
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main">
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
+
+export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Box, Container } from "@mui/system";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./Header/Header";
 
 const Layout = () => {
   return (
     <Box>
       <Container>
+        <Header />
         <Outlet />
       </Container>
     </Box>
